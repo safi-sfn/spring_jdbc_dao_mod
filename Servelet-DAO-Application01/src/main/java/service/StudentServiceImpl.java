67ba934fe76ce1c724dbefc7305b36bb740c0749
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-03 12:32:52 +0530

    feat(service) : student service and its implementation

---
+++ Servelet-DAO-Application01/src/main/java/service/StudentServiceImpl.java
@@ -0,0 +1,38 @@
+package service;
+
+import dao.StudentDao;
+import dao.StudentDaoImpl;
+import dto.Student;
+import factory.StudentDaoFactory;
+import service.StudentService;
+
+public class StudentServiceImpl implements StudentService {
+
+	private StudentDao studentDao;
+	String status = "";
+	@Override
+	public String addStudent(Student std) {
+		studentDao = StudentDaoFactory.getStudentDao();
+		status = studentDao.add(std);
+		return status;
+	}
+
+	@Override
+	public Student searchStudent(String sid) {
+		studentDao = StudentDaoFactory.getStudentDao();
+		Student std = studentDao.search(sid);
+		return std;
+	}
+
+	@Override
+	public String deleteStudent(String sid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+
+	
+
+}

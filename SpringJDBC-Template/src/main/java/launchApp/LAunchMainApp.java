e7116cbd1d2336e1951184d1e24a9471053270d3
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-04 17:11:36 +0530

    feat(main) : final commit

--- SpringJDBC-Template/src/main/java/launchApp/LAunchMainApp.java
+++ SpringJDBC-Template/src/main/java/launchApp/LAunchMainApp.java
@@ -12,6 +12,7 @@ public class LAunchMainApp {
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationConfig.xml");
 		IEmployeeDao employeeDao = (IEmployeeDao)context.getBean("employeeDao");
+	/*
 		Employee emp = new Employee();
 		emp.setEid(112);
 		emp.setEname("Gulam");
@@ -19,6 +20,37 @@ public class LAunchMainApp {
 		emp.setEsallary(50000);
 		String status = employeeDao.add(emp);
 		System.out.println(status);
+	*/
+		
+	/*	
+		Employee emp = employeeDao.search(114);
+		if(emp == null) {
+			System.out.println("Employee not existed");
+		}
+		else {
+			System.out.println("Employee detail");
+			System.out.println("---------------------");
+			System.out.println("Employee ID      : "+emp.getEid());
+			System.out.println("Employee Name    : "+emp.getEname());
+			System.out.println("Employee Sallary : "+emp.getEsallary());
+			System.out.println("Employee City    : "+emp.getEcity());
+		}
+	*/
+		
+	/*
+		Employee emp = new Employee();
+		emp.setEid(112);
+		emp.setEname("Gustavo");
+		emp.setEcity("ENG");
+		emp.setEsallary(12000);
+		String status = employeeDao.update(emp);
+		System.out.println(status);
+	*/
+		
+		String status = employeeDao.delete(114);
+		System.out.println(status);
+		
+		
 	}
 
 }

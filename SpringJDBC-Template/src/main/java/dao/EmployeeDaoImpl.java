1e1b17438ee3d74e394747d21735b28e72929114
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-04 17:12:25 +0530

    feat(dao) : CRUD operation completed by using JDBC Template

--- SpringJDBC-Template/src/main/java/dao/EmployeeDaoImpl.java
+++ SpringJDBC-Template/src/main/java/dao/EmployeeDaoImpl.java
@@ -55,20 +55,73 @@ public class EmployeeDaoImpl implements IEmployeeDao {
 
 	@Override
 	public Employee search(int eid) {
-		// TODO Auto-generated method stub
-		return null;
+		Employee emp = null;
+		try {
+			List<Employee> empList = jdbcTemplate.query("select * from Employee where eId = "+eid, 
+					(rs,index) ->{
+						Employee emp1 = new Employee();
+						emp1.setEid(rs.getInt("eId"));
+						emp1.setEname(rs.getString("eName"));
+						emp1.setEsallary(rs.getFloat("eSallary"));
+						emp1.setEcity(rs.getString("eCity"));
+						return emp1;
+			});
+			
+			if(empList.isEmpty() == true) {
+				return null;
+			}else {
+				emp = empList.get(0);
+			}
+				
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return emp;
 	}
 
 	@Override
 	public String update(Employee emp) {
-		// TODO Auto-generated method stub
-		return null;
+		String status = "";
+		try {
+			Employee emp1 = search(emp.getEid());
+			if(emp1 == null) {
+				status = "Employee not existed";
+			}else {
+				int rowCount = jdbcTemplate.update("update Employee set eName = '"+emp.getEname()+"', eSallary = "+emp.getEsallary()+", eCity = '"+emp.getEcity()+"' where eId = "+emp.getEid());         
+				if(rowCount == 1) {
+					status = "employee updated successfully";
+				}else {
+					status = "employee updation failure";
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("employee updation failure");
+			e.printStackTrace();
+		}
+		return status;
 	}
 
 	@Override
 	public String delete(int eid) {
-		// TODO Auto-generated method stub
-		return null;
+		String status = "";
+		try {
+			Employee emp = search(eid);
+			if(emp == null) {
+				status = "Employee not existed";
+			}else {
+				int rowCount = jdbcTemplate.update("delete from Employee where eId = "+eid);
+				if(rowCount == 1) {
+					status = "Deletion success";
+				}
+				else {
+					status = "Deleation failure";
+				}
+			}
+		} catch (Exception e) {
+			status = "Deletion failure";
+			e.printStackTrace();
+		}
+		return status;
 	}
 
 }

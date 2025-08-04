ada02adcf2e9eb057d2d8af8dc2f2ed8257c9557
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-04 13:55:56 +0530

    feat(dao): implement employee add operation with using spring JDBC template

---
+++ SpringJDBC-Template/src/main/java/dao/EmployeeDaoImpl.java
@@ -0,0 +1,66 @@
+package dao;
+
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+
+import dto.Employee;
+import mapper.EmployeeRowMapper;
+
+//STEP-JDBC- 2. preparing DAO interface Implementation class
+
+public class EmployeeDaoImpl implements IEmployeeDao {
+
+	// 2.a Declare JDBC Template Property and respective Setter
+	private JdbcTemplate jdbcTemplate;
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+	
+
+
+	@Override
+	
+	public String add(Employee emp) {
+		
+		String status="";
+		try {
+			List<Employee> empList = jdbcTemplate.query("SELECT * FROM Employee WHERE eId = "+emp.getEid(), new EmployeeRowMapper());
+			if(empList.isEmpty() == true) {
+				int rowCount = jdbcTemplate.update("INSERT INTO Employee VALUES(" + emp.getEid() + ",'" + emp.getEcity() + "','" + emp.getEname() + "'," + emp.getEsallary() + ")");     
+				if(rowCount == 1) {
+					status="Employee inserted successfull";
+				}else {
+					status="Employee insertion failure";
+				}
+			}else {
+				status = "Employee Existed Already";
+			}
+		} catch (Exception e) {
+			status="Employee insertion failure";
+			e.printStackTrace();
+		}
+		
+		return status;
+	}
+
+	@Override
+	public Employee search(int eid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String update(Employee emp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String delete(int eid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

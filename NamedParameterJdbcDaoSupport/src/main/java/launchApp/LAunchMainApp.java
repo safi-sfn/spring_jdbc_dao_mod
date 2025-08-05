49530ab7eba74cf11b560a36fdf214646759ed65
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-05 18:48:07 +0530

    refactor

--- NamedParameterJdbcDaoSupport/src/main/java/launchApp/LAunchMainApp.java
+++ NamedParameterJdbcDaoSupport/src/main/java/launchApp/LAunchMainApp.java
@@ -124,46 +124,7 @@ public class LAunchMainApp {
 			}
 		}
 		
-		
-		
-		
-	/*
-		Employee emp = new Employee();
-		emp.setEid(112);
-		emp.setEname("Gulam");
-		emp.setEcity("HYD");
-		emp.setEsallary(50000);
-		String status = employeeDao.add(emp);
-		System.out.println(status);
-	*/
-		
-	/*	
-		Employee emp = employeeDao.search(114);
-		if(emp == null) {
-			System.out.println("Employee not existed");
-		}
-		else {
-			System.out.println("Employee detail");
-			System.out.println("---------------------");
-			System.out.println("Employee ID      : "+emp.getEid());
-			System.out.println("Employee Name    : "+emp.getEname());
-			System.out.println("Employee Sallary : "+emp.getEsallary());
-			System.out.println("Employee City    : "+emp.getEcity());
-		}
-	*/
-		
-	/*
-		Employee emp = new Employee();
-		emp.setEid(112);
-		emp.setEname("Gustavo");
-		emp.setEcity("ENG");
-		emp.setEsallary(12000);
-		String status = employeeDao.update(emp);
-		System.out.println(status);
-	*/
-		
-//		String status = employeeDao.update(112);
-//		 System.out.println(status);
+
 		
 		
 	}

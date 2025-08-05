a93fa7903cc471e21d815faba60020814419e318
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-05 17:28:03 +0530

    feat(dto) : employee dto class

---
+++ JdbcDaoSupport/src/main/java/dto/Employee.java
@@ -0,0 +1,37 @@
+package dto;
+
+public class Employee {
+	private int eid;
+	private String ename;
+	private float esal;
+	private String ecity;
+	
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public float getEsal() {
+		return esal;
+	}
+	public void setEsal(float esal) {
+		this.esal = esal;
+	}
+	public String getEcity() {
+		return ecity;
+	}
+	public void setEcity(String ecity) {
+		this.ecity = ecity;
+	}
+	
+	
+	
+	
+}

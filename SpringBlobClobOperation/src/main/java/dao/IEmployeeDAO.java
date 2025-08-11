575aa76467a9720abab4ffeeff1f81da38195e2d
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-11 13:15:16 +0530

    feat(dao) : set parameter eno to readEmployee()

--- SpringBlobClobOperation/src/main/java/dao/IEmployeeDAO.java
+++ SpringBlobClobOperation/src/main/java/dao/IEmployeeDAO.java
@@ -4,5 +4,5 @@ import dto.EmployeeDTO;
 
 public interface IEmployeeDAO {
 	public void insertEmployee(EmployeeDTO emp);
-	public EmployeeDTO readEmployee();
+	public EmployeeDTO readEmployee(int eno);
 }
